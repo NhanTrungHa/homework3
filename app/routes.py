bbc2f8091715b87debfa9cc1d67176e792e4714c
Nhan <nhantrungha@gmail.com>
2022-03-21 14:55:47 -0700

    Test

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, flash
 
 # create an instance of Flask class
 # __name__ is a predefined setup variable
-myapp_obj = Flask(__name__)
+myobj = Flask(__name__)
 
 name = 'Nhan'
 city_names = ['San Jose', 'Los Angeles', 'Tokyo']
 
 # different URL the app will implement
-@myapp_obj.route("/")
+@myobj.route("/")
 # called view function
 def home():
     return render_template('home.html', city_names=city_names, name=name)

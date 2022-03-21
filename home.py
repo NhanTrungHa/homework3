498a4bca6ae85c40e8e66606cefeff4f162b7dcf
Nhan <nhantrungha@gmail.com>
2022-03-21 13:53:53 -0700

    home

--- home.py
+++ home.py
@@ -19,13 +19,13 @@ def home():
     </head>
     <body>
         <h1>Welcome ''' + name + '''</h1>
-        
+
         <a href="https://www.google.com">not google</a>
-        
+
         <ul> 
-            <li>'''+ city_names[0] + '''</li>
-            <li>'''+ city_names[1] + '''</li>
-             <li>'''+ city_names[2] + '''</li>
+            <li>''' + city_names[0] + '''</li>
+            <li>''' + city_names[1] + '''</li>
+             <li>''' + city_names[2] + '''</li>
          </ul>
     </body>
     </html>'''

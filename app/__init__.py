213fe65d6d98de0a23e9706e2de368e3ae27a9ab
Nhan <nhantrungha@gmail.com>
2022-03-21 16:58:12 -0700

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import Config
 myobj = Flask(__name__)
 
 myobj.config.from_mapping(
-    SECRET_KEY= 'you - will - never - guess'
+    SECRET_KEY='you - will - never - guess'
 )
 
 from app import routes

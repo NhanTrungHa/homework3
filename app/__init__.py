d04076d5bffe1766f7a7a7b417ad8640d60d51fb
Nhan <nhantrungha@gmail.com>
2022-03-21 15:51:56 -0700

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
+from config import Config
 
-myobj = Flask(__name__)
+app = Flask(__name__)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,27 @@
 # include Flask class from file flask
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, request
 
 # create an instance of Flask class
 # __name__ is a predefined setup variable
-myapp_obj = Flask(__name__)
+myobj = Flask(__name__)
 
 name = 'Nhan'
 city_names = ['San Jose', 'Los Angeles', 'Tokyo']
 
+
 # different URL the app will implement
-@myapp_obj.route("/")
+@myobj.route("/")
 # called view function
-def home():
+def home(*args):
     return render_template('home.html', city_names=city_names, name=name)
 
-#myapp_obj.run()
+
+@myobj.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        city = request.form["nm"]
+        return redirect("/", city=city)
+    else:
+        return render_template("login.html")
+
+myobj.run()

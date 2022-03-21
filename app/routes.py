c06a45a0a36f01762914e46eba41a757b547c4f0
Nhan <nhantrungha@gmail.com>
2022-03-21 15:23:06 -0700

    Test

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,17 @@
 # include Flask class from file flask
 from flask import Flask, render_template, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    textbox = StringField('City Name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
 
 # create an instance of Flask class
 # __name__ is a predefined setup variable
-myobj = Flask(__name__)
+myapp_obj = Flask(__name__)
 
 name = 'Nhan'
 city_names = ['San Jose', 'Los Angeles', 'Tokyo']
 
-
 # different URL the app will implement
-@myobj.route("/")
+@myapp_obj.route("/")
 # called view function
 def home():
-    form = LoginForm()
-    return render_template('home.html', city_names=city_names, name=name, form=form)
+    return render_template('home.html', city_names=city_names, name=name)
 
-
-#myobj.run()
+#myapp_obj.run()

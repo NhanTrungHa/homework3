7f4f4512baf32d351588fe7dfac0521092f9e3d7
Nhan <nhantrungha@gmail.com>
2022-03-21 17:04:01 -0700

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,6 @@ myobj.config.from_mapping(
     SECRET_KEY='you - will - never - guess'
 )
 
+myobj.config['SECRET_KEY'] = 'you - will - never - guess'
+myobj.secret_key = 'you - will - never - guess'
 from app import routes
